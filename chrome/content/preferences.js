a1d2e66dc406babdfd8d55a855282886d9a0ba83
Christian Höltje <docwhat@gerf.org>
2007-02-11 20:45:03 -0500

    * better layout for the preferences, Yay!

--- chrome/content/preferences.js
+++ chrome/content/preferences.js
@@ -1,6 +1,4 @@
 // @todo [6] [pref] Better strategy for getting the default editor: EDITOR env variable or view_source.editor.path
-// @todo [3] [pref] Preference dialog redesign
-// @todo [1] [pref] [bug] The help text seems to be wrong a lot.
 /**
  * Open a filepicker to select the value of the editor.
  */
@@ -55,11 +53,10 @@ function pref_onload() {
         while (box.firstChild) {
             box.removeChild(box.firstChild);
         }
-        var desc, textnode;
-        desc = document.createElement('description');
-        textnode = document.createTextNode(["I was unable to run your editor, '",
-                                            editor.value,
-                                            "'.  Use the browse button to choose another editor and try again. "].join(''));
+        var desc = document.createElement('description');
+        var textnode = document.createTextNode(["I was unable to run your editor, '",
+                                                editor.value,
+                                                "'.  Use the browse button to choose another editor and try again. "].join(''));
         desc.appendChild(textnode);
         desc.style.maxWidth = '18em';
         box.appendChild(desc);
