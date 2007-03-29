6bbe2eff2ebaf9add01deb396d3020629812b323
docwhat@gerf.org <docwhat@gerf.org>
2007-03-29 08:10:41 -0400

    added todo item

--- chrome/content/preferences.js
+++ chrome/content/preferences.js
@@ -1,4 +1,5 @@
 // @todo [6] [pref] Better strategy for getting the default editor: EDITOR env variable or view_source.editor.path
+// @todo [8] [pref] Option to make the textarea uneditable when using editor.
 
 /**
  * Open a filepicker to select the value of the editor.
