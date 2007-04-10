c03b525bb1d56c827e951dafc79101c23015bde9
docwhat@gerf.org <docwhat@gerf.org>
2007-04-10 13:15:15 -0400

    changed the refresh rate to be lower.

--- src/defaults/preferences/itsalltext.js
+++ src/defaults/preferences/itsalltext.js
@@ -1,7 +1,7 @@
 pref("extensions.itsalltext.lastversion",  "");
 pref("extensions.itsalltext.charset",  "UTF-8");
 pref("extensions.itsalltext.editor",   "");
-pref("extensions.itsalltext.refresh",  7);
+pref("extensions.itsalltext.refresh",  3);
 pref("extensions.itsalltext.debug",  false);
 pref("extensions.itsalltext.disable_gumdrops", false);
 pref("extensions.itsalltext.extensions",  '.txt,.html,.css,.xml,.xsl,.js');
